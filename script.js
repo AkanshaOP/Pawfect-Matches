1a5e74afabd8c888e723bc000b85b6c34a2c0cac
AkanshaOP <166599972+AkanshaOP@users.noreply.github.com>
2025-01-19 18:15:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.getElementById("signUpButton").addEventListener("click", function () {
+    alert("Thank you for signing up!");
+  });
+
+  // JavaScript code for download functionality
+  document.getElementById("downloadButton").addEventListener("click", function () {
+    alert("Download will start shortly!");
+  });
